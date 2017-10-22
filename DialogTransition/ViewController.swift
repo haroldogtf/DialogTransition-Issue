772b154c55d5ba97ac2e68ebfa1f38a8ca6c25df
Haroldo Gondim <haroldo.gondim@gmail.com>
2017-10-22 10:28:30 -0300

    initial commit

---
+++ DialogTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DialogTransition
+//
+//  Created by Haroldo Gondim on 22/10/17.
+//  Copyright © 2017 Haroldo Gondim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
